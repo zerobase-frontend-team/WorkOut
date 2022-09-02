c1667d0d4ae7220215e4b1e220afd10de6a1849d
Lee-SoYoon <99dlthdbs@gmail.com>
2022-09-02 15:11:04 +0900

    Update ⚡ tailwind 반응형 설정 및 Router 설정

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,10 +1,19 @@
-import React from "react";
-import { Route, Router, Routes } from "react-router-dom";
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from '../routes/Home';
+import Reservation from '../routes/Reservation';
 
-function AppRouter(){
-    return <Routes>
-        <Route></Route>
-    </Routes>
+function AppRouter() {
+  return (
+    <main>
+      <Routes>
+        <Route path="/reservation" element={<Reservation />}></Route>
+        <Route path="/schedule" element={<Reservation />}></Route>
+        <Route path="/myinfo" element={<Reservation />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </main>
+  );
 }
 
 export default AppRouter;
---
+++ src/routes/Reservation.tsx
@@ -0,0 +1,7 @@
+function Reservation(){
+    return <div className="sm:w-1 lg:w-8 bg-black">
+        Reservation
+    </div>
+}
+
+export default Reservation;
