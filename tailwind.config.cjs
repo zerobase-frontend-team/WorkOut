9b1cb234ceb7ac8eb203ef36bddd8cb8d9c040d8
Lee-SoYoon <99dlthdbs@gmail.com>
2022-09-03 18:23:04 +0900

    Update ⚡ tailwindcss 및 daisyui 재설치

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-    },
   },
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: ['emerald'],
-  },
+  plugins: [],
 };
