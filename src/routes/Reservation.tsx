57d34e633838bba6c23c8060a911d62c512656bf
Lee-SoYoon <99dlthdbs@gmail.com>
2022-09-05 16:02:05 +0900

    BugFixed 🐛 tailwindcss 안되는 오류 수정 (postcss 추가)

--- src/routes/Reservation.tsx
+++ src/routes/Reservation.tsx
@@ -1,10 +1,27 @@
-function Reservation(){
-    return <section>
-        <section className="bg-green-500 md:bg-red-500 lg:bg-green-500">
-            Reservation
+import { FunctionComponent } from 'react';
+import Calendar from '../components/Calendar';
+
+interface ReservationProps {}
+
+const Reservation: FunctionComponent<ReservationProps> = () => {
+  return (
+    <section>
+      <section className="">
+        Reservation
+        <section className="Calendar flex items-center bg-green-500 md:bg-red-500 lg:bg-green-500">
+          <div>
+            <div className="flex">
+              <div>&lt;</div>
+              <div>{}</div>
+              <div>&gt;</div>
+            </div>
+          </div>
+          <div className="week"></div>
+          <div className="dates"></div>
         </section>
-        
+      </section>
     </section>
-}
+  );
+};
 
 export default Reservation;
